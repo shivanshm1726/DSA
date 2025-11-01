00f02b23ac7ae9a9d0c5600ce9e31e1d1bc622b8
Shivansh Mishra <shivanshm665@gmail.com>
2025-11-02 01:28:27 +0530

    update fibonacci

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,8 +1,12 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1){
-            return n;
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = b;
+            b = a + b;
+            a = temp;
         }
-        return fib(n-1) + fib(n-2);
+        return b;
     }
 }
