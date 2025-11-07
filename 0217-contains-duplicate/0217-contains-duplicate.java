435de05b82e9d78945e07b516dfe2ef48834f11b
Shivansh Mishra <shivanshm665@gmail.com>
2025-11-08 01:10:44 +0530

    Time: 24 ms (8.02%), Space: 76.4 MB (9.18%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i-1] == arr[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
