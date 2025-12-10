612c4af2e97d4f5add7bcc3092feabaae4d87bb7
Shivansh Mishra <shivanshm665@gmail.com>
2025-12-10 23:27:12 +0530

    Time: 2 ms (93.61%), Space: 47.6 MB (52.87%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,23 +1,17 @@
 class Solution {
-    public int[] findErrorNums(int[] arr) {
-        int i = 0;
-        while(i<arr.length){
-            int correct = arr[i] - 1;
-            if(arr[i] != arr[correct]){
-                int temp = arr[i];
-                arr[i] = arr[correct];
-                arr[correct] = temp;
-            }else{
-                i++;
-            }
+    public int[] findErrorNums(int[] nums) {
+        int[] freq = new int[nums.length + 1];
+        int dup = -1, miss = -1;
+
+        for(int x : nums){
+            freq[x]++;
         }
 
-        for(int index = 0; index<arr.length; index++){
-            if(arr[index] != index+1){
-                return new int[] {arr[index], index+1};
-            }
+        for(int i = 1; i<=nums.length; i++){
+            if(freq[i] == 2) dup = i;
+            if(freq[i] == 0) miss = i;
         }
 
-        return new int[] {1,1};
+        return new int[]{dup, miss};
     }
 }
