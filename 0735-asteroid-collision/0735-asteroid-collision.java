33847ed785ffc865339fa45db282df30c558bbc9
Shivansh Mishra <shivanshm665@gmail.com>
2026-02-14 01:55:48 +0530

    Time: 4 ms (81.42%), Space: 47.1 MB (56.86%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i<asteroids.length; i++){
+            if(stack.isEmpty() || asteroids[i] > 0){
+                stack.push(asteroids[i]);
+            }else{
+                while(!stack.isEmpty()){
+                    int top = stack.peek();
+                    if(top < 0){
+                        stack.push(asteroids[i]);
+                        break;
+                    }
+                    int modVal = Math.abs(asteroids[i]);
+                    if(modVal == top){
+                        stack.pop();
+                        break;
+                    }else if(modVal < top){
+                        break;
+                    }else{
+                        stack.pop();
+                        if(stack.isEmpty()){
+                            stack.push(asteroids[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int len = stack.size();
+        int[] arr = new int[len];
+        for(int i = len-1; i>=0; i--){
+            arr[i] = stack.pop();
+        }
+
+        return arr;
+    }
+}
