a61c1aae1c61dd8ea5266c69ea09bf80fcea5461
Shivansh Mishra <shivanshm665@gmail.com>
2026-06-25 02:31:37 +0530

    Time: 1 ms (100%), Space: 43.8 MB (41.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,17 +1,16 @@
 class Solution {
     public int myAtoi(String s) {
         int n = s.length();
+        int i = 0;
         int sign = 1;
         int ans = 0;
-        int i = 0;
 
         //1.
-        while(i < n && s.charAt(i) == ' '){
+        while(i < n && (s.charAt(i) == ' ')){
             i++;
         }
 
         //2.
-
         if(i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')){
             if(s.charAt(i) == '-'){
                 sign = -1;
@@ -19,19 +18,18 @@ class Solution {
             i++;
         }
 
-        //3. 
-        while(i < s.length() && Character.isDigit(s.charAt(i))){
+        //3.
+        while(i < n && Character.isDigit(s.charAt(i))){
             int digit = s.charAt(i) - '0';
-
-            //Overflow check
-            if(ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && digit > 7)){
+            //overflow check
+            if(ans > Integer.MAX_VALUE / 10 || ans == Integer.MAX_VALUE / 10 && digit > 7){
                 if(sign == 1){
                     return Integer.MAX_VALUE;
                 }else{
                     return Integer.MIN_VALUE;
                 }
             }
-            ans = (ans * 10) + digit;
+            ans = ans * 10 + digit;
             i++;
         }
 
