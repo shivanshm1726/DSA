2addde15cd3e536a7ac3a71ceff8a2e3cdb6a139
Shivansh Mishra <shivanshm665@gmail.com>
2026-06-27 15:34:38 +0530

    Time: 17 ms (77.76%), Space: 45.4 MB (65%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -5,7 +5,7 @@ class Solution {
         int closestSum = arr[0] + arr[1] + arr[2];
 
         for(int i = 0; i< n; i++){
-            if(i > 0 && arr[i] == arr[i-1]) continue;
+            
             int l = i + 1;
             int r = n - 1;
 
